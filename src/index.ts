6ce9693376745ffa01842342f23a243431f1300c
Alex Mendes <alexanderhmendes@gmail.com>
2022-09-17 22:11:23 +0100

    fix: JestOpenApiCoverageConfig export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { globalSetup } from './global-setup';
 export { globalTeardown } from './global-teardown';
 export { requestInterceptor } from './request/interceptor';
 export { writeDocs } from './docs/io';
-export { Config } from './config/openapi';
+export { JestOpenApiCoverageConfig } from './config/openapi';
